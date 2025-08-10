a969b440d6c5969831f5eacef27c58947dc426a7
ohrytskov <ohrytskov20230516@gmail.com>
2025-08-10 17:51:34 +0000

    refactor(data): extract sample posts and feed into src/data

--- src/data/sampleFeed.js
+++ src/data/sampleFeed.js
@@ -1,8 +1,4 @@
-// components/home/Feed.jsx
-import { useState } from 'react';
-import FeedPost from './FeedPost';
-import FeedFilterBar from './FeedFilterBar'
-import sampleFeed from '@/data/sampleFeed'
+const sampleFeed = [
   {
     id: 1,
     avatar: '/images/feed/avatar1.svg',
@@ -76,50 +72,5 @@ But instead of sending another “just checking in” email 📩 , I sent a shor
   },
 ];
 
-export default function Feed() {
-  const [followed, setFollowed] = useState({});
-  const [selectedTags, setSelectedTags] = useState([]);
-  const [sortBy, setSortBy] = useState('Best');
-  const toggleFollow = (id) => {
-    setFollowed((prev) => ({ ...prev, [id]: !prev[id] }));
-  };
+export default sampleFeed
 
-  // derive list of all tags and filter posts by selected tags
-  const availableTags = Array.from(new Set(sampleFeed.flatMap((post) => post.tags || [])));
-  const filteredPosts = sampleFeed.filter(
-    (post) =>
-      selectedTags.length === 0 ||
-      (post.tags || []).some((tag) => selectedTags.includes(tag))
-  );
-  // sort posts according to selected sort option
-  const parseTimestamp = (ts) => {
-    const val = parseInt(ts, 10) || 0;
-    return ts.includes('h') ? val * 60 : val;
-  };
-  const sortedPosts = [...filteredPosts];
-  if (sortBy === 'New') {
-    sortedPosts.sort((a, b) => parseTimestamp(a.timestamp) - parseTimestamp(b.timestamp));
-  } else if (sortBy === 'Top') {
-    sortedPosts.sort((a, b) => b.likes - a.likes);
-  } else if (sortBy === 'Rising') {
-    sortedPosts.sort((a, b) => b.commentsCount - a.commentsCount);
-  }
-
-  return (
-    <div className="flex flex-col ">
-      <FeedFilterBar
-        availableTags={availableTags}
-        selectedTags={selectedTags}
-        onChange={setSelectedTags}
-        sortBy={sortBy}
-        onSortChange={setSortBy}
-      />
-      {sortedPosts.map((post) => (
-        <FeedPost key={post.id} {...post} onFollow={() => toggleFollow(post.id)} />
-      ))}
-
-      {/* Rounded footer */}
-      <div className="w-[684px] h-4 relative bg-white border border-t-0 mt-[-1px] mb-10 border-gray-200 rounded-bl-2xl rounded-br-2xl" />
-    </div>
-  );
-}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,70 +8,12 @@ import Menu from '@/components/home/Menu'
 
 import PostCarousel from '@/components/home/PostCarousel'
 import Feed from '@/components/home/Feed'
+import samplePosts from '@/data/samplePosts'
 import SuggestedUsers from '@/components/home/SuggestedUsers'
 import { signOut } from 'firebase/auth'
 import { auth } from '@/firebase/config'
 
-const samplePosts = [
-  {
-    id: 0,
-    bgImage: '/images/4.svg',
-    avatar: 'https://placehold.co/40x40',
-    username: 'david.sdr',
-    likes: '12k likes',
-    comments: '128 comments',
-    caption: 'Closed my biggest deal after a month.',
-    sourceLogo: '/images/logo3.svg',
-    sourceName: 'SalesSniper',
-  },
-  {
-    id: 1,
-    bgImage: '/images/1.svg',
-    avatar: 'https://placehold.co/40x40',
-    username: 'jane.sells.alot',
-    likes: '5k likes',
-    comments: '88 comments',
-    caption:
-      'Sales is 10% selling, 90% refreshing your inbox and pretending to be unbothered.',
-    sourceLogo: '/images/logo1.svg',
-    sourceName: 'VentureGrid',
-  },
-  {
-    id: 2,
-    bgImage: '/images/2.svg',
-    avatar: 'https://placehold.co/40x40',
-    username: 'smith.john',
-    likes: '3k likes',
-    comments: '45 comments',
-    caption:
-      'I’m emotionally attached to leads that ghosted me 3 weeks ago. I still believe in us.',
-    sourceLogo: '/images/logo2.svg',
-    sourceName: 'Salesgrowth',
-  },
-  {
-    id: 3,
-    bgImage: '/images/3.svg',
-    avatar: 'https://placehold.co/40x40',
-    username: 'andrea.bdev7',
-    likes: '3k likes',
-    comments: '45 comments',
-    caption:
-      'Sales is 10% selling, 90% refreshing your inbox and pretending to be unbothered.',
-    sourceLogo: '/images/logo2.svg',
-    sourceName: 'Salesgrowth',
-  },
-  /*{
-    id: 4,
-    bgImage: '/images/4.svg',
-    avatar: 'https://placehold.co/40x40',
-    username: 'david.sdr',
-    likes: '12k likes',
-    comments: '128 comments',
-    caption: 'Closed my biggest deal after a month.',
-    sourceLogo: '/images/logo3.svg',
-    sourceName: 'SalesSniper',
-  },*/
-]
+
 
 
 export default function Home() {
