8946042f77548c5a92a22010620ed34a96bfdef3
ohrytskov <ohrytskov20230516@gmail.com>
2025-08-10 14:27:37 +0300

    style(FollowStep): adjust input width calculation for better responsiveness

--- src/components/FollowStep.jsx
+++ src/components/FollowStep.jsx
@@ -57,7 +57,7 @@ export default function FollowStep({
             value={search}
             onChange={setSearch}
             label={searchLabel}
-            className="w-full mt-6"
+            className="w-[calc(100%-24px)] mt-6"
             rounded="full"
             rightElement={(
               <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
