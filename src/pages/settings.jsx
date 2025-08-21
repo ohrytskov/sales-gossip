0737e5928fdf0ddbe3d45d22b161d405c865f0ad
ohrytskov <ohrytskov20230516@gmail.com>
2025-08-21 17:58:06 +0000

    feat(settings): add settings page

---
+++ src/pages/settings.jsx
@@ -0,0 +1,12 @@
+import Header from '@/components/Header'
+
+export default function Settings() {
+  return (
+    <div className="min-h-screen w-screen">
+      <Header />
+      <main className="mx-auto max-w-5xl p-6">
+        <h1 className="text-2xl font-medium text-slate-900">Settings</h1>
+      </main>
+    </div>
+  )
+}
