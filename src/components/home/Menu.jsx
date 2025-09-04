700745d1849072621cbf9ac60c9736b739e1e39c
ohrytskov <ohrytskov20230516@gmail.com>
2025-09-04 05:56:05 +0000

    menu: wrap menu items with Next Link

--- src/components/home/Menu.jsx
+++ src/components/home/Menu.jsx
@@ -1,4 +1,5 @@
 
+import Link from 'next/link'
 import Gossips from './menu/Gossips'
 import Companies from './menu/Companies'
 import Tags from './menu/Tags'
@@ -22,23 +23,24 @@ export default function Menu({ selectedTab, onSelect }) {
       {menuItems.map(({ key, label, Icon }) => {
         const isActive = selectedTab === key
         return (
-          <div
-            key={key}
-            onClick={() => onSelect(key)}
-            className={`flex flex-col items-center cursor-pointer ${
-              isActive ? 'text-pink-900' : 'text-gray-400'
-            }`}
-          >
-            <Icon />
-            <span>{label}</span>
-            <div
-              className={`
-                w-16 h-0 border-b-2 
-                ${isActive ? 'border-pink-900' : 'border-transparent'}
-                mt-[11px] mb-[-11px] 
-              `}
-            />
-          </div>
+          <Link key={key} href="/" legacyBehavior>
+            <a
+              onClick={() => onSelect(key)}
+              className={`flex flex-col items-center ${
+                isActive ? 'text-pink-900' : 'text-gray-400'
+              }`}
+            >
+              <Icon />
+              <span>{label}</span>
+              <div
+                className={`
+                  w-16 h-0 border-b-2 
+                  ${isActive ? 'border-pink-900' : 'border-transparent'}
+                  mt-[11px] mb-[-11px] 
+                `}
+              />
+            </a>
+          </Link>
         )
       })}
     </nav>
