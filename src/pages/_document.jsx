3b88a1227fd1b96944da1c568886de8f6e67db69
ohrytskov <ohrytskov20230516@gmail.com>
2026-03-02 23:29:36 +0000

    chore: preload fonts

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,11 +1,17 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
+const GOOGLE_FONTS_HREF =
+  'https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700;900&family=Inter:wght@400;500;600&display=swap';
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="preload" as="style" href={GOOGLE_FONTS_HREF} />
         <link
-          href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700;900&family=Inter:wght@400;500;600&display=swap"
+          href={GOOGLE_FONTS_HREF}
           rel="stylesheet"
         />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
